d041fe08aeab178ba9056ddacbf928776dfefd65
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-10-26 12:01:49 -0300

    Create models/Insert data/Add custom methods

---
+++ app/models/like.rb
@@ -0,0 +1,12 @@
+class Like < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+
+    #Callback
+    after_save :updates_post_likes_counter
+
+    #Methods
+    def updates_post_likes_counter
+        post.update(likes_counter: post.likes.count)
+    end    
+end
