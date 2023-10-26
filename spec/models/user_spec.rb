6afedc5e0b8a38a4b96973fdd680bb2421cf7de4
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-10-26 19:40:07 -0300

    unit rspec for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+   subject { User.new(name: 'John Doe') } 
+
+   before { subject.save }
+
+   describe '#validations' do
+
+   it 'name muts not be blank' do 
+    subject.name = nil
+    expect(subject).to_not be_valid
+   end
+
+   it 'PostsCounter must be an integer greater than or equal to zero' do
+    subject.post_counter = -1
+    expect(subject).to_not be_valid
+   end
+end
+
+   describe '#three_most_recent_post' do
+    it 'should return three most recent posts' do 
+        user = User.create(name: 'Jonh Doe', post_counter: 0)
+        post1 = Post.create(title: 'post1', author_id: user.id, comments_counter: 0, likes_counter: 0)
+        post2 = Post.create(title: 'post2', author_id: user.id, comments_counter: 0, likes_counter: 0)
+        post3 = Post.create(title: 'post3', author_id: user.id, comments_counter: 0, likes_counter: 0)
+        post4 = Post.create(title: 'post4', author_id: user.id, comments_counter: 0, likes_counter: 0)
+
+        recents_posts = user.three_most_recent_post
+
+        expect(recents_posts).to eq([post4, post3, post2])
+    end
+end
+end
