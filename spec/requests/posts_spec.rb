9b0069e32687f2f451fe376d0ef984f11dfec251
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-10-31 14:32:22 -0300

    Add spec for post

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'Jonh Doe', post_counter: 0) }
+  let(:post) {Post.create(title: 'Test post', author_id: user.id, comments_counter: 0, likes_counter: 0) }
+
+  describe 'GET #index' do
+
+  before do
+    get user_posts_path(user)
+   end
+
+    it 'response status was correct' do
+    expect(response).to have_http_status(:success)
+    end
+
+    it 'correct template was rendered' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include("posts")
+    end
   end
+
+  describe 'GET #show' do
+
+  before do
+   get user_post_path(user, post)
+  end  
+
+    it 'response status was correct' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'correct template was rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include("specific")
+    end
+  end  
 end
