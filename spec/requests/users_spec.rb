5f58240e9a972b30c9ffef8631a4d7d13dbfe3e2
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-10-31 14:32:14 -0300

    Add spec for users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'Jonh Doe', post_counter: 0) }
+  
+  describe 'GET #index' do
+    it 'response status was correct' do
+      get users_path
+    expect(response).to have_http_status(:success)
+    end
+
+    it 'correct template was rendered' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body includes correct placeholder text' do
+      get users_path
+      expect(response.body).to include("list")
+    end
   end
+
+  describe 'GET #show' do
+
+    it 'response status was correct' do
+      get user_path(user)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'correct template was rendered' do
+      get user_path(user)
+      expect(response).to render_template(:show)
+    end
+
+    it 'response body includes correct placeholder text' do
+      get user_path(user)
+      expect(response.body).to include("specific")
+    end
+  end  
 end
